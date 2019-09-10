5a937cdf977f27887fe3f4bb42437d2460fa2f6c
olivier_12 <oliviergiroud1998@gmail.com>
2019-09-10 17:05:21 +0700

    Adding Intent with feedback value from one Activity (ResultActivity)

--- app/src/main/java/com/example/christianpranata/myintentapp/MainActivity.java
+++ app/src/main/java/com/example/christianpranata/myintentapp/MainActivity.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    TextView tvResult;
+    private int REQUEST_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +28,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnDialPhone = findViewById(R.id.btn_dial_number);
         btnDialPhone.setOnClickListener(this);
+
+        Button btnMoveForResult = findViewById(R.id.btn_move_for_result);
+        btnMoveForResult.setOnClickListener(this);
+
+        tvResult = findViewById(R.id.tv_result);
     }
 
     @Override
@@ -56,6 +64,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person);
                 startActivity(moveWithObjectIntent);
                 break;
+            case R.id.btn_move_for_result:
+                Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
+                startActivityForResult(moveForResultIntent, REQUEST_CODE);
+                break;
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == MoveForResultActivity.RESULT_CODE) {
+                int selectedValue = data.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0);
+                tvResult.setText(String.format("Hasil : %s", selectedValue));
+            }
         }
     }
 }
